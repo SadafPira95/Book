90c48916f7d2057318360313404f5ed1ae8ed1ff
SadafPira <sadaf.6674@gmail.com>
2025-07-19 03:06:57 +0330

    Add files via upload

---
+++ Coordinates Exercise-Chapter2.dart
@@ -0,0 +1,18 @@
+typedef touchHandler =void Function(double x, double y);
+
+class surface{
+  touchHandler onTouch;
+  surface(this.onTouch);
+  void touch(double x,double y) {
+    if(onTouch!=null){
+      onTouch(x,y);
+    }
+  }
+}
+
+void main() {
+  surface Surface=surface((double x,double y) {
+    print('Touch at: ($x,$y)');
+  });
+  Surface.touch(334, 443.64);
+}
